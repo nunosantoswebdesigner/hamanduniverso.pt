064ee28f7e5eeaa0815cb1a94ee742333e4c4e96
Nuno Santos <42045537+nunosantoswebdesigner@users.noreply.github.com>
2024-11-08 14:27:21 +0000

    Add files via upload

--- src/components/Testemunhos.jsx
+++ src/components/Testemunhos.jsx
@@ -12,7 +12,7 @@ const Testemunhos = () => {
         <Heading className="md:max-w-md lg:max-w-2xl" title="Os nossos Clientes dizem" tag="Testemunhos" />
         <div className="flex flex-wrap justify-center gap-10 mb-10">
           {benefits.map((item) => (
-            <div className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]" style={{ backgroundImage: `url(${clienteCard})`, }} key={item.id}>
+            <div className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]" style={{ backgroundImage: `url(${item.backgroundUrl})`, }} key={item.id}>
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
                 <div className="flex gap-3">
                   <img src={item.img} alt="reviewer" className="w-12 h-12 rounded-full" />
